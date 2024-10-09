471ac42f8a5b33f49d5d3243473e888130383b55
dhaval-chavda2006 <dhavalchavda866@gmail.com>
2024-10-09 20:44:16 +0530

    FACTOR OF NO.

---
+++ factors.c
@@ -0,0 +1,19 @@
+ // factors of given no.
+
+#include<stdio.h>
+void main(){
+	
+	int n,i;
+	
+	printf("ENTER NO: ");
+	scanf("%d",&n);
+	
+	i=1;
+while(i<=n){
+	
+	if(n%i==0){
+		printf("\n%d",i);
+	}
+	i++;	
+}
+}
