23fe72625cb76346fe52f2ec33571c4c5ab0720f
dhaval-chavda2006 <dhavalchavda866@gmail.com>
2024-10-17 14:11:14 +0530

    PATTERN OF * 5x5

---
+++ pattern_5x5star.c
@@ -0,0 +1,21 @@
+// pattern of column = 5(j), row = 5(i)
+/*	*****
+	*****
+	*****
+	*****
+	*****	
+	        */
+
+#include<stdio.h>
+void main(){
+	 int i,j;
+	 
+for(i=1;i<=5;i++){
+	
+	for(j=1;j<=5;j++){
+		
+		printf("*");
+	}
+	printf("\n");
+}
+}
