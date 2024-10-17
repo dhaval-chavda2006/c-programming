e738aeb042bebf844a0184a042bdac8c04c3f888
dhaval-chavda2006 <dhavalchavda866@gmail.com>
2024-10-17 14:31:53 +0530

    5x5 pattern of 1

---
+++ pattern_5x5_1.c
@@ -0,0 +1,21 @@
+// pattern of column = 5(j), row = 5(i)
+/*	11111
+	1111
+	111
+	11
+	1	
+	        */
+
+#include<stdio.h>
+void main(){
+	 int i,j;
+	 
+for(i=1;i<=5;i++){
+	
+	for(j=1;j<=5;j++){
+		
+		printf("1");
+	}
+	printf("\n");
+}
+}
