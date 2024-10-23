eace98304e8039a7818e01563ecfd12db1499d3c
dhaval-chavda2006 <dhavalchavda866@gmail.com>
2024-10-23 22:32:02 +0530

    11111 2222 333 44 5

---
+++ pattern_12345.c
@@ -0,0 +1,20 @@
+/*pattern 11111
+		  2222
+		  333
+		  44
+		  5*/	
+
+#include<stdio.h>
+
+int main() {
+    int i, j;
+
+        for (i = 1; i <= 5; i++) {
+        
+        for (j = 5; j >= i; j--) {
+            printf("%d", i);
+        }
+        
+        printf("\n");
+    }
+}
